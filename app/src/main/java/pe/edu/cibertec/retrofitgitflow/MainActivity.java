7f37eee1dd01aca799274a9cc60ead1cb6b08bd5
Miguel Almeyda Vasquez <estebanermn@gmail.com>
2019-06-30 12:08:39 -0500

    version 1

--- app/src/main/java/pe/edu/cibertec/retrofitgitflow/MainActivity.java
+++ app/src/main/java/pe/edu/cibertec/retrofitgitflow/MainActivity.java
@@ -3,12 +3,66 @@ package pe.edu.cibertec.retrofitgitflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewResult = findViewById(R.id.textViewResult);
+        callService();
+    }
+
+    private void callService()
+    {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHoderApi jsonPlaceHoderApi = retrofit.create(JsonPlaceHoderApi.class);
+
+        Call<List<Post>> call = jsonPlaceHoderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful())
+                {
+                    textViewResult.setText("Code: " + response.code());
+                }
+                else
+                {
+                    List<Post> pots = response.body();
+                    for (Post post : pots)
+                    {
+                        String content = "";
+                        content += "Id: " + post.getId() + "\n";
+                        content += "userId: " + post.getUserId() + "\n";
+                        content += "Title: " + post.getTitle() + "\n";
+                        content += "Body: " + post.getText() + "\n";
+                        textViewResult.append(content);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+                t.printStackTrace();
+            }
+        });
     }
 }
